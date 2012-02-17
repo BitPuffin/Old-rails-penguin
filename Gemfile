bc32c31642b4384b3953afda8efa3a41a8b6bbf6
Isak Andersson <BitPuffin@lavabit.com>
2012-02-18 00:38:04 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'rails', '~> 3.1.3'
+
+gem 'scrypt'
+
+gem 'unicorn'
+
+gem 'ripple', :git => 'git://github.com/seancribbs/ripple.git'
+
+gem 'excon'
+
+gem 'yajl-ruby'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Penguin::Application.config.secret_token = '3636f4b0bfff813d86ae83dff9fe3739fb7d22b5f23c48abeabd9389c9b1395bc6fff229d5f1823a6f18ff437f78d32c7bd1695fb7331e78b8eddd424a5e4d22'
